aed2c5674585f51a7f39e0bfddf5c87d482e783c
AlwinJacobThomas <kailath.ajt@gmail.com>
2020-10-11 07:04:39 +0530

    added index.html,script.js,style.css,hotspot.html,bg.png

---
+++ script.js
@@ -0,0 +1,10 @@
+function opennav(){
+    document.getElementById("sidenav").style.width="250px";
+    document.getElementById("header").style.filter = "brightness(93%)";
+    document.getElementById("section").style.filter = "brightness(93%)";
+    }
+function closenav(){
+    document.getElementById("sidenav").style.width="0%";
+    document.getElementById("header").style.filter = "brightness(100%)";
+    document.getElementById("section").style.filter = "brightness(100%)";
+}
